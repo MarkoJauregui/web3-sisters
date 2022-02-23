0ab00f2ea7b214ab04d99b1c989a35657158f0de
Misty-Dev <markojau@gmail.com>
2022-02-23 16:12:57 -0300

    intro and assets

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import './intro.scss';
 export default function Intro() {
 	return (
 		<div className="intro" id="intro">
-			intro
+			<div className="left">
+				<div className="wrapper">
+					<h1>
+						Connecting artists to opportunities <br /> in the Web3 economy.{' '}
+					</h1>
+				</div>
+				<a href="#description">
+					<img src="assets/downarrow.png" alt="down arrow" />
+				</a>
+			</div>
+			<div className="right">
+				<div className="imgContainer">
+					<img src="assets/web3logoblanco2.png" alt="white logo" />
+				</div>
+			</div>
 		</div>
 	);
 }
